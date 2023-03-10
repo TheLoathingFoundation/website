09f65dda519098e11ccf4834802455f8b81c0c7b
Dan Schultz <slifty@gmail.com>
2023-03-10 12:34:46 -0500

    Set up everything
    This commit is too big but YOLO.
    
    It sets up svelte, defines the initial landing page + layout, and set up
    the github action to auto-publish to github pages.
    
    Issue #7

--- README.md
+++ README.md
@@ -1,5 +1,30 @@
-# The Loathing Foundation (name TBD)
+# The Loathing Foundation
 
 A co-op model for a capitalism simulator in the game [Kingdom of Loathing](https://www.kingdomofloathing.com).
 
 Please read the current state of [The Idea](docs/IDEA.md) for more information.
+
+## Developing
+
+1. Installing dependencies
+
+`yarn install`
+
+2. Starting th dev server
+
+```bash
+npm run dev
+
+# or start the server and open the app in a new browser tab
+npm run dev -- --open
+```
+
+## Building
+
+To create a production version:
+
+```bash
+npm run build
+```
+
+You can preview the production build with `npm run preview`.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,75 @@
+<div id="mission">
+	The Loathing Foundation provides <em>affordable access</em> to items sold in <a href="https://kol.coldfront.net/thekolwiki/index.php/Mr._Store" target="_blank" rel="noreferrer">Mr. Store</a> for players who want to <em>actually use them</em>.
+</div>
+
+<h2>Wait, What?</h2>
+
+<p>The Loathing Foundation is a philanthropic community project from the <a href="https://discord.gg/kol" target="_blank" rel="noreferrer">Ascension Speed Society</a>. We provide a way for players to potentially access <a href="https://museum.loathers.net/player/3580284" target="_blank" rel="noreferrer">past items of the month</a> without having to spend billions of real world dollars.</p>
+
+<h2>How does it work?</h2>
+
+<p>Each month we invite some number of members to purchase one of the items that our loathing economists have deemed "available for distribution".</p>
+<p>Selected players will be able to trade <em>1 Mr. A + 3 Uncle Bucks for one "available" item of their choice</em>.</p>
+<p>The player gets the item, and The Loathing Foundation gets currency to buy future Items of the Month to add to the pool of future items!</p>
+
+<h2>Is this some kind of scam?</h2>
+
+<p>No!</p>
+
+<h2>What's the catch?</h2>
+
+<p>If you obtain an item through The Loathing Foundation <em>you must use it</em>.</p>
+<p><em><b>Do not sell it</b> -- and don't sell copies of it in the future</em>.</p>
+<p>If we see a copy of an item you got from us in your mall store then you will have made us all very sad AND your membership to the foundation will be permanently revoked, leaving you forever ineligable to make future trades.</p>
+
+<h2>How do I participate?</h2>
+
+<div id="join">
+	<a href="https://forms.gle/Z63oLzmFCPFf4sDb7" target="_blank" rel="noreferrer">Sign Up</a>
+</div>
+
+<h2>How did you get all this stuff, some kind of infinite meat bug?</h2>
+
+<p>All items in The Loathing Foundation have been generously donated by members of the community (or purchased using the funds gained from the trade-in process).</p>
+
+<h2>Can I donate?</h2>
+
+<p>Absolutely! Send any donations via kmail to <b>The Loathing Foundation (#3580284)</b></p>
+<p>Please note: <em>We do NOT deal with meat, only Mr. Items. </em></p>
+
+
+<style>
+	#mission {
+		font-size: 2em;
+		font-family: arial;
+		border: 2px solid black;
+		padding: 15px;
+		line-height: 1.5em;
+	}
+
+	#join a {
+		display: block;
+		border: 3px solid #0e0067;
+		padding: 30px;
+		font-weight: bold;
+		font-size: 30px;
+		width: 300px;
+		text-align: center;
+		margin: 30px auto;
+		text-decoration: none;
+		color: black;
+		background-color: #cccefb;
+	}
+	em {
+		font-style: normal;
+		background-color: #ffffb2;
+	}
+	h2 {
+		margin-bottom: 0px;
+		margin-top: 30px;
+	}
+	p {
+		margin: 10px 0px;
+		line-height: 1.4em;
+	}
+</style>
